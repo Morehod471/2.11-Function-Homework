c13d33e4306658d6d5d4f56311bbde41d90c6f78
Homework2 <ivliev.k@yandex.ru>
2023-03-19 12:36:49 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    public static <T> void findMinMax(
+        Stream<? extends T> stream,
+        Comparator<? super T> order,
+        BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        List<T> list = stream.collect(Collectors.toList());
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            list.sort(order);
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        }
+    }
+
+    public static void countEvenNums(List<Integer> list) {
+        Stream<Integer> strean = list.stream();
+        int count = (int) list.stream().filter(n -> n % 2 == 0).count();
     }
 }
