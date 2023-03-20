6c0c072d4eb74f9142f4071294af2a8678219c80
Homework2 <ivliev.k@yandex.ru>
2023-03-20 20:58:37 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,34 @@
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<Integer> listNum = new ArrayList<Integer>();
+        listNum.add(15);
+        listNum.add(7);
+        listNum.add(12);
+        listNum.add(2);
+        listNum.add(8);
+        listNum.add(104);
+        listNum.add(86);
+        listNum.add(45);
+        listNum.add(20);
+        listNum.add(57);
+
+        Stream<Integer> numStream = listNum.stream();
+        Comparator<Integer> comparator1 = Comparator.comparingInt(Integer::intValue);
+        BiConsumer<Integer, Integer> biConsumer = (min, max) -> System.out.println(min + " " + max);
+
+        findMinMax(numStream, comparator1, biConsumer);
+
+        System.out.println(countEvenNums(listNum));
+        System.out.println( );
     }
 
     public static <T> void findMinMax(
@@ -13,17 +36,17 @@ public class Main {
         Comparator<? super T> order,
         BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        List<T> list = stream.collect(Collectors.toList());
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
         if (list.isEmpty()) {
             minMaxConsumer.accept(null, null);
         } else {
-            list.sort(order);
             minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
         }
     }
 
-    public static void countEvenNums(List<Integer> list) {
+    public static Integer countEvenNums(List<Integer> list) {
         Stream<Integer> strean = list.stream();
         int count = (int) list.stream().filter(n -> n % 2 == 0).count();
+        return count;
     }
 }
